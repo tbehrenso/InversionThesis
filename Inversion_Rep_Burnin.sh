ab2808d5d307185c9afe4f5b5571c135c1391f72
Thomas Behrens <tbehrens@binfservms01.unibe.ch>
2022-10-17 14:59:01 +0200

    Fixed batch script directory issues

--- Inversion_Rep_Burnin.sh
+++ Inversion_Rep_Burnin.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem-per-cpu=4000M
 #SBATCH --time=8:00:00
 #SBATCH --job-name=slimjob
-#SBATCH --output=/data/users/tbehrens/inv_2022/slim_output/slim_%j.o
-#SBATCH --error=/data/users/tbehrens/inv_2022/errors_slim/error_slim_%j.e
+#SBATCH --output=/data/users/tbehrens/InversionThesis/slim_output/slim_%j.o
+#SBATCH --error=/data/users/tbehrens/InversionThesis/errors_slim/error_slim_%j.e
 #SBATCH --mail-user=thomas.behrens@students.unibe.ch
 #SBATCH --mail-type=fail
 
@@ -18,11 +18,11 @@ m=0.001
 mu=1e-6
 nrep=100
 
-sim_type=neutral_2pop
+sim_type=inversionLAA_2pop
 dir_name=${sim_type}_s${s}_m${m}_mu${mu}
 
 mkdir Outputs/${dir_name}
-mkdir Outputs/${dir_name}/{5000,6000,7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000}
+mkdir Outputs/${dir_name}/{5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000}
 
 for r in $(seq 1 $nrep)
 do
