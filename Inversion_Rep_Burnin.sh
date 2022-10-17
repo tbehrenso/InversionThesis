15a896819141f31eb18ae23b2f2bcb3927e7b2b6
Thomas Behrens <tbehrens@binfservms01.unibe.ch>
2022-10-17 14:45:56 +0200

    Extended batch script for extended sim

--- Inversion_Rep_Burnin.sh
+++ Inversion_Rep_Burnin.sh
@@ -16,13 +16,13 @@ module load Development/gcc/9.2.1
 s=0.01
 m=0.001
 mu=1e-6
-nrep=200
+nrep=100
 
 sim_type=neutral_2pop
 dir_name=${sim_type}_s${s}_m${m}_mu${mu}
 
 mkdir Outputs/${dir_name}
-mkdir Outputs/${dir_name}/{5000,6000,7000}
+mkdir Outputs/${dir_name}/{5000,6000,7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000}
 
 for r in $(seq 1 $nrep)
 do
